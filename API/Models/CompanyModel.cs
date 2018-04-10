bf7a2fb26a15e0a6bb24419d9c77f07fa5ac4203
200360407 <200360407.student.georgianc.on.ca>
2018-04-09 20:54:30 -0400

    first commit

---
+++ API/Models/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class CompanyModel : DbContext
+    {
+        // constructor
+        public CompanyModel(DbContextOptions<CompanyModel> options) : base(options) { }
+    }
+}
---
+++ API/Models/child.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("child")]
+    public class child
+    {
+
+        [Key]
+        [StringLength(10)]
+        public string color2 { get; set; }
+
+        [StringLength(10)]
+        public string location { get; set; }
+
+        [StringLength(2000)]
+        public string Description { get; set; }
+    }
+}
